9e810ca01c828c61b25b6c9e453dabd4ff984d3d
Pavel <steshinpavel14@gmail.com>
2022-02-23 18:07:46 +0600

    Add currency switcher in `main.js`

--- js/main.js
+++ js/main.js
@@ -36,4 +36,45 @@ $(function () {
         $('.sideMenu__text').toggleClass('sideMenu__text--active');
         $('.sideMenu__ico-img').toggleClass('sideMenu__ico-img--active');
     });
+
+    $('.post__transfer-value').on('click', function () {
+        $('.post__transfer-value_list').toggleClass('post__transfer-value_list--active')
+    })
+
+    //USD 
+
+    $('.post__transfer-value-us__list').on('click', function () {
+        $('.post__transfer-value-ru--active').removeClass('post__transfer-value-ru--active').addClass('post__transfer-value-ru--nactive');
+        $('.post__transfer-value-kz--active').removeClass('post__transfer-value-kz--active').addClass('post__transfer-value-kz--nactive');
+        $('.post__transfer-value-us--nactive').removeClass('post__transfer-value-us--nactive').addClass('post__transfer-value-us--active');
+
+        $('.post__transfer-value-us_list--active').removeClass('post__transfer-value-us_list--active').addClass('post__transfer-value-us_list--nactive');
+        $('.post__transfer-value-kz_list--nactive').removeClass('post__transfer-value-kz_list--nactive').addClass('post__transfer-value-kz_list--active');
+        $('.post__transfer-value-ru_list--nactive').removeClass('post__transfer-value-ru_list--nactive').addClass('post__transfer-value-ru_list--active');
+    })
+
+    //KZT
+
+    $('.post__transfer-value-kz__list').on('click', function () {
+        $('.post__transfer-value-ru--active').removeClass('post__transfer-value-ru--active').addClass('post__transfer-value-ru--nactive');
+        $('.post__transfer-value-us--active').removeClass('post__transfer-value-us--active').addClass('post__transfer-value-ru--nactive');
+        $('.post__transfer-value-kz--nactive').removeClass('post__transfer-value-kz--nactive').addClass('post__transfer-value-kz--active');
+
+        $('.post__transfer-value-kz_list--active').removeClass('post__transfer-value-kz_list--active').addClass('post__transfer-value-kz_list--nactive');
+        $('.post__transfer-value-en_list--nactive').removeClass('post__transfer-value-en_list--nactive').addClass('post__transfer-value-en_list--active');
+        $('.post__transfer-value-ru_list--nactive').removeClass('post__transfer-value-ru_list--nactive').addClass('post__transfer-value-ru_list--active');
+    })
+
+    //RUB
+
+    $('.post__transfer-value-ru__list').on('click', function () {
+        $('.post__transfer-value-us--active').removeClass('post__transfer-value-us--active').addClass('post__transfer-value-us--nactive');
+        $('.post__transfer-value-kz--active').removeClass('post__transfer-value-kz--active').addClass('post__transfer-value-kz--nactive');
+        $('.post__transfer-value-ru--nactive').removeClass('post__transfer-value-ru--nactive').addClass('post__transfer-value-ru--active');
+
+        $('.post__transfer-value-kz_list--nactive').removeClass('post__transfer-value-kz_list--nactive').addClass('post__transfer-value-kz_list--active');
+        $('.post__transfer-value-en_list--nactive').removeClass('post__transfer-value-en_list--nactive').addClass('post__transfer-value-en_list--active');
+        $('.post__transfer-value-ru_list--active').removeClass('post__transfer-value-ru_list--active').addClass('post__transfer-value-ru_list--nactive');
+    })
+
 });
